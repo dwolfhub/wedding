24da4796e234fb69d2451bbe15d41970d0b2835a
Daniel Wolf <danielrwolf5@gmail.com>
2016-10-25 14:02:08 -0500

    pages, photos, content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
-console.log('ok');
+
+
+(function () {
+    /* TABS */
+    var sections = document.querySelectorAll('section');
+    var sectionButtons = document.querySelectorAll('.menu a');
+    var tabButtonClicked = function (event) {
+        event.preventDefault();
+
+        for (var i = 0; i < sectionButtons.length; i++) {
+            sectionButtons[i].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        for (var i = 0; i < sections.length; i++) {
+            sections[i].classList.remove('active');
+        }
+
+        var selector = '#section-' + this.href.split('#').pop();
+        document.querySelector(selector).classList.add('active');
+    };
+
+    for (var i = 0; i < sectionButtons.length; i++) {
+        sectionButtons[i].addEventListener('click', tabButtonClicked);
+    }
+})();
