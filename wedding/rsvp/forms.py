8fb2d03d8df60749c03be81c9af7aed9bf235e80
Daniel Wolf <danielrwolf5@gmail.com>
2017-01-13 15:51:04 -0600

    design updates

--- wedding/rsvp/forms.py
+++ wedding/rsvp/forms.py
@@ -1,4 +1,56 @@
 from django import forms
+from django.utils.encoding import force_text
+from django.utils.html import format_html
+
+
+class DropdownChoiceInput(forms.widgets.ChoiceInput):
+    input_type = 'dropdown'
+
+    def __init__(self, *args, **kwargs):
+        super(DropdownChoiceInput, self).__init__(*args, **kwargs)
+        self.value = set(force_text(v) for v in self.value)
+
+    def is_checked(self):
+        return self.choice_value in self.value
+
+    def render(self, name=None, value=None, attrs=None):
+        coming, not_coming = '', ''
+        if self.is_checked():
+            coming = 'selected="selected"'
+        else:
+            not_coming = 'selected="selected"'
+
+        return format_html(
+            '<label>{}</label>'
+            '<div class="select-wrapper">'
+            '<select name="people-{}">'
+            '<option value="" {}>cannot attend</option>'
+            '<option value="{}" {}>will be attending</option>'
+            '</select>'
+            '</div><div class="clearfix"></div>'.format(self.choice_label, self.index, not_coming, self.choice_value, coming)
+        )
+
+
+class DropdownFieldRenderer(forms.widgets.ChoiceFieldRenderer):
+    choice_input_class = DropdownChoiceInput
+
+    outer_html = '<div{id_attr} class="form-width-wrapper">{content}</div>'
+    inner_html = '{choice_value}{sub_widgets}'
+
+
+class DropdownSelectMultiple(forms.widgets.RendererMixin, forms.SelectMultiple):
+    renderer = DropdownFieldRenderer
+
+    def value_from_datadict(self, data, files, name):
+        if isinstance(data, forms.widgets.MultiValueDict):
+            people = []
+            for key, val in data.items():
+                if 'people-' in key and val:
+                    people.append(val)
+            return people
+        else:
+            return data.get(name)
+
 
 
 class PersonForm(forms.Form):
@@ -30,7 +82,7 @@ class PeopleForm(forms.Form):
         widget=forms.HiddenInput,
     )
     people = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple(),
+        widget=DropdownSelectMultiple(),
         required=False
     )
     message = forms.CharField(
